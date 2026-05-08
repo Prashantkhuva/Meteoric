2d71d206b724aa0ab1f9b40e38061200cc12e2ea
Prashantkhuva <work.prashantkhuva@gmail.com>
2026-05-08 18:43:39 +0530

    feat: update Open Graph image dimensions for improved social sharing

--- src/seo.config.js
+++ src/seo.config.js
@@ -3,8 +3,8 @@ export const SITE_NAME = "Meteoric";
 export const DEFAULT_OG_IMAGE = "/og-image.png?v=20260508";
 
 /** PNG dimensions for public/og-image.png used by og:image / Twitter. */
-export const OG_IMAGE_WIDTH = 1200;
-export const OG_IMAGE_HEIGHT = 630;
+export const OG_IMAGE_WIDTH = 1635;
+export const OG_IMAGE_HEIGHT = 962;
 
 export const OG_LOCALE = "en_US";
 
