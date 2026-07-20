34c5ae1d2b4575a98aa17f84dcd48973efe9cb2a
Prashantkhuva <work.prashantkhuva@gmail.com>
2026-07-20 19:58:40 +0530

    fix the navabr bg issue

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -120,7 +120,7 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="fixed md:static top-0 left-0 w-full max-w-full z-50 py-4 md:pt-6 bg-transparent md:bg-transparent backdrop-blur-[20px] md:backdrop-blur-none overflow-hidden">
+      <header className="fixed md:static top-0 left-0 w-full max-w-full z-50 py-4 md:pt-6 bg-transparent overflow-hidden">
         <div
           className="flex items-center justify-between w-full max-w-7xl mx-auto px-6 md:px-[72px]"
         >
